a2e8c2be332bbb570b4eb946803f283a8a688a0e
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2017-03-24 08:28:48 +0800

    Improvements

---
+++ Backtester.Server/Controllers/Orders/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using Backtester.Server.ControllerUtils;
+using Backtester.Server.Models;
+using Capital.GSG.FX.Utils.Core.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Backtester.Server.Controllers.Orders
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly ILogger logger = GSGLoggerFactory.Instance.CreateLogger<OrdersController>();
+
+        private readonly JobsControllerUtils utils;
+
+        public OrdersController(JobsControllerUtils utils)
+        {
+            this.utils = utils;
+        }
+
+        public async Task<IActionResult> Index(string jobId, int orderId)
+        {
+            var job = await utils.Get(jobId);
+
+            if (job?.Output?.Orders?.Count > 0 && job.Output.Orders.ContainsKey(orderId))
+                return View(job.Output.Orders[orderId].ToOrderModel());
+            else
+                return View();
+        }
+    }
+}
---
+++ Backtester.Server/ViewModels/Orders/OrdersListPartialViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Backtester.Server.Models;
+using System.Collections.Generic;
+
+namespace Backtester.Server.ViewModels.Orders
+{
+    public class OrdersListPartialViewModel
+    {
+        public List<BacktestOrderModel> Orders { get; private set; }
+        public string JobId { get; private set; }
+
+        public OrdersListPartialViewModel(string jobId, List<BacktestOrderModel> orders)
+        {
+            JobId = jobId;
+            Orders = orders ?? new List<BacktestOrderModel>();
+        }
+    }
+}
