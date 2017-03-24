8144fecca7f0d727685f4739d047392f304ed754
Guillaume Teirlinck <guillaume.teirlinck@gsgfintech.com>
2017-03-24 11:20:53 +0800

    Improvements

--- Backtester.Server/ViewModels/Orders/OrdersListPartialViewModel.cs
+++ Backtester.Server/ViewModels/Orders/OrdersListPartialViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Backtester.Server.Models;
+using Capital.GSG.FX.Utils.Core;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Backtester.Server.ViewModels.Orders
 {
@@ -8,10 +10,29 @@ namespace Backtester.Server.ViewModels.Orders
         public List<BacktestOrderModel> Orders { get; private set; }
         public string JobId { get; private set; }
 
+        public string ClipboardContent { get; private set; }
+
         public OrdersListPartialViewModel(string jobId, List<BacktestOrderModel> orders)
         {
             JobId = jobId;
             Orders = orders ?? new List<BacktestOrderModel>();
+
+            SetupClipboardContent();
+        }
+
+        private void SetupClipboardContent()
+        {
+            if (!Orders.IsNullOrEmpty())
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var order in Orders)
+                    sb.AppendLine($"{order.OrderId},{order.PlacedTime},{order.LastUpdateTime},{order.Origin},{order.Status},{order.Type},{order.Side},{order.Quantity},{order.Cross},{order.FillPrice},{order.LimitPrice},{order.StopPrice},{order.TrailingAmount}");
+
+                ClipboardContent = sb.ToString();
+            }
+            else
+                ClipboardContent = string.Empty;
         }
     }
 }
--- Backtester.Server/ViewModels/Trades/TradesListPartialViewModel.cs
+++ Backtester.Server/ViewModels/Trades/TradesListPartialViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Backtester.Server.Models;
+using Capital.GSG.FX.Utils.Core;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Backtester.Server.ViewModels.Trades
 {
@@ -8,10 +10,29 @@ namespace Backtester.Server.ViewModels.Trades
         public string JobGroupId { get; private set; }
         public List<BacktestTradeModel> Trades { get; private set; }
 
+        public string ClipboardContent { get; private set; }
+
         public TradesListPartialViewModel(string jobGroupId, List<BacktestTradeModel> trades)
         {
             JobGroupId = jobGroupId;
-            Trades = trades;
+            Trades = trades ?? new List<BacktestTradeModel>();
+
+            SetupClipboardContent();
+        }
+
+        private void SetupClipboardContent()
+        {
+            if (!Trades.IsNullOrEmpty())
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var trade in Trades)
+                    sb.AppendLine($"{trade.Timestamp},{trade.OrderOrigin},{trade.Side},{trade.Size},{trade.Cross},{trade.Price},{trade.RealizedPnlUsd},{trade.RealizedPnlPips},{trade.Duration},{trade.CommissionUsd}");
+
+                ClipboardContent = sb.ToString();
+            }
+            else
+                ClipboardContent = string.Empty;
         }
     }
 }
